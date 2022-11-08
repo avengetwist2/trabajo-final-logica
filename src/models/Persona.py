b1658837a7154547b1ce11765c0b4d505d72b2d4
unknown <canarruz21@hotmail.com>
2022-11-08 18:43:46 -0500

    creacion de clases modelos

--- src/models/Persona.py
+++ src/models/Persona.py
@@ -1,13 +1,45 @@
 
 
 class Persona:
-
     def __init__(self, nombre, correo, telefono, rol, password):
-        self.nombre=nombre
+        self.__nombre=nombre
         self.correo=correo
         self.telefono=telefono
         self.rol=rol
         self.password=password
-@
-    def __str__(self):
-        return [self.nombre, self.correo, self.telefono, self.rol]
+
+    # aqui van los metodos de acceso a los atributos
+    def getNombre(self):
+        return self.__nombre
+
+
+    # aqui van los metodos para cambiar el valor de los atributos
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+
+
+    def __toString__(self):
+        return f"""
+        nombre = {self.__nombre}
+        correo = {self.correo}
+        password = {self.password}
+        """
+
+
+
+
+
+
+
+
+tomarNombre = Persona("brayan","brayan@gmail.com","3124567", "admin", "123")
+print(tomarNombre.getNombre())
+tomarNombre.setNombre('juan')
+print(tomarNombre.getNombre())
+
+
+
+
+
+
+
